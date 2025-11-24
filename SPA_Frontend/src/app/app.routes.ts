2a052d6f64e8d524bc4fbc3f6ff1096d8bc47fc2
Cade-Bray <bray.cade@gmail.com>
2025-11-23 23:38:48 -0700

    Add Angular CLI and Node.js run configurations, update routing and navigation links

--- SPA_Frontend/src/app/app.routes.ts
+++ SPA_Frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {Login} from './login/login';
+import {IotListing} from './iot-listing/iot-listing';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'home', component: IotListing},
   {path: 'login', component: Login},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: '**', redirectTo: 'login'}
 ];
