98c706d6184f66e60be34d90b013185182f571fb
Cade-Bray <bray.cade@gmail.com>
2026-02-14 16:13:06 -0700

    Implement authentication methods and JWT generation in IoT device schema

--- Node_Backend/models/iot.js
+++ Node_Backend/models/iot.js
@@ -1,16 +1,53 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 // Define the device schema
 const iotDeviceSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    state: {type: String, required: true},
+    state: {type: String},
     currentTemp: {type: Number},
-    setTemp: {type: Number, required: true},
-    lastChecked: {type: Date, required: true},
-    hash: {type: String, required: false},
-    salt: {type: String, required: false},
-    auth_users: {type: Array, required: true}
+    setTemp: {type: Number},
+    lastChecked: {type: Date, default: Date.now()},
+    hash: {type: String},
+    salt: {type: String},
+    auth_users: {type: Array}
 });
+
+// Method to set the secret on this record.
+iotDeviceSchema.methods.setSecret = function(secret){
+    this.salt = crypto.randomBytes(16).toString();
+    this.hash = crypto.pbkdf2Sync(
+        secret,
+        this.salt,
+        1000,
+        64,
+        'sha512'
+    ).toString();
+};
+
+// Method to validate the secret on this record against the hash
+iotDeviceSchema.methods.validSecret = function (secret) {
+    const hash = crypto.pbkdf2Sync(
+        secret,
+        this.salt,
+        1000,
+        64,
+        'sha512'
+    ).toString();
+    return this.hash === hash;
+};
+
+// Method to generate the JWT
+iotDeviceSchema.methods.generateJWT = function () {
+    return jwt.sign(
+        { // Payload for our JSON Web Token
+            _id: this._id
+        },
+        process.env.JWT_SECRET, // Secret stored in the .env file
+        {expiresIn: '1m'} // Short-lived token for microtransactions.
+    );
+};
 
 const iotDevice = mongoose.model('iot', iotDeviceSchema, 'iot');
 module.exports = iotDevice;
