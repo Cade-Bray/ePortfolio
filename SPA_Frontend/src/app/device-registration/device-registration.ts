cf6a1a19c7f4db7519190bda0d3e3da74dbdd54b
Cade-Bray <bray.cade@gmail.com>
2026-02-26 14:25:06 -0700

    Add device registration component with HTML, CSS, and TypeScript files

---
+++ SPA_Frontend/src/app/device-registration/device-registration.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-device-registration',
+  imports: [],
+  templateUrl: './device-registration.html',
+  styleUrl: './device-registration.css',
+})
+export class DeviceRegistration {
+
+}
